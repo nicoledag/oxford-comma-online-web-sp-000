616ddc59cdf8669a53339068798d779d09731324
Nicole D <nnardella@gmail.com>
2018-10-19 17:44:30 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,15 +1,12 @@
-require 'pry'
+
 def oxford_comma(array)
 #array = ["a", "b", "c"]
-
-
 if array.length == 1
   array.join()
 elsif array.length == 2
   array.join(" and ")
 else
 last = array.pop()
-binding.pry
   array.join(", ") + ", and " + "#{last}"
 end
 
