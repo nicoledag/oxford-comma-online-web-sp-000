856972fefc5a418f7d1f6ac69e7cd9dc53b5cb0d
Nicole D <nnardella@gmail.com>
2018-10-19 17:39:08 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,13 +1,15 @@
 require 'pry'
 def oxford_comma(array)
 #array = ["a", "b", "c"]
-binding.pry
+
+
 if array.length == 1
   array.join()
 elsif array.length == 2
   array.join(" and ")
 else
 last = array.pop()
+binding.pry
   array.join(", ") + ", and " + "#{last}"
 end
 
